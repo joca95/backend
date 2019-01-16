3855f5f4444ab65ccdcc284bcb866ce114e83ae2
kensystem <kenmel28@gmail.com>
2019-01-15 23:54:57 -0500

    Entity user create

---
+++ src/main/java/com/usersapp/model/User.java
@@ -0,0 +1,104 @@
+package com.usersapp.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+@Access(AccessType.FIELD)
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 700906554966125688L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false, unique = true)
+	private int id;
+
+	@Column(name = "primer_nombre", nullable = false, length = 100)
+	private String primerNombre;
+
+	@Column(name = "segundo_nombre", nullable = false, length = 100)
+	private String segundoNombre;
+
+	@Column(name = "apellido_paterno", nullable = false, length = 100)
+	private String apellidoPaterno;
+
+	@Column(name = "apellido_materno", nullable = false, length = 100)
+	private String apellidoMaterno;
+
+	@Column(name = "telefono", nullable = false, length = 20)
+	private String telefono;
+
+	@Column(name = "direccion", nullable = false, length = 100)
+	private String direccion;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
+	}
+
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
